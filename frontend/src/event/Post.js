d7fc1ed32e29eaca049809f198dfd721a39ab6cd
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-05-05 18:55:01 +0200

    WIP Add post pop up with comments

--- frontend/src/event/Post.js
+++ frontend/src/event/Post.js
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
 import { Button, Card, Col, Modal, Row } from "react-bootstrap";
 import "./Post.css";
+
+export default function Post({ id, title, desc, votes, image }) {
+  const [comments, setComments] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const get = async () => {
+    await fetch(`http://localhost:8000/comment?post=${id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        return response.json();
+      })
+      .then((data) => {
+        setComments(data.results);
+        console.log(data.results);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+
   return (
     <>
       <Row
         className="justify-content-center my-3 post"
         key={id}
+        onClick={handleShow}
       >
         <Col sm={9} md={8}>
           <Card>
@@ -22,6 +52,17 @@ import "./Post.css";
           </Card>
         </Col>
       </Row>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{desc}</Modal.Body>
+        <Modal.Footer>
+          {comments.map((comment) => (
+            <p>{comment.desc}</p>
+          ))}
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
